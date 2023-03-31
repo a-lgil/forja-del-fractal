d3648b114b394c719881cf0321ba9f0c92d28813
ALUPM <aalvvarro4@gmail.com>
2023-03-31 14:37:09 +0200

    Fix undefined interpolate variable

--- sfi.py
+++ sfi.py
@@ -351,7 +351,7 @@ with viewer_col:
         start_time = time.time()
 
         fractal = dibujar_fractal(sistema_funciones_iteradas(ifs, pasos, semilla), color, show_points, show_lines,
-                                    point_size if show_points else None, line_width if show_lines else None, interpolate)
+                                    point_size if show_points else None, line_width if show_lines else None, interpolate if show_lines else None)
 
         end_time = time.time()
 
